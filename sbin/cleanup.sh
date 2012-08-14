3aae7d84d7f898c39f8f046d34a566abf3c83cc6
Lior Galanti <lior.galanti@gmail.com>
2012-08-14 13:59:41 -0400

    javadoc

--- sbin/cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo '(cd '$1'; tar --remove-files -cf '$2'.arg.tar *.arg.gz)'|bash
-echo '(cd '$1'; tar --remove-files -czf '$2'.mgr.tgz *.mgr)'|bash
-echo '(cd '$1'; tar --remove-files -czf '$2'.log.tgz log)'|bash
-echo '(cd '$1'; rm -rf *.stats)'|bash
-echo '(cd '$1'; rm -rf log)'|bash
-
--- sbin/extract.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-INPUT=$1
-PATTERN=$2
-OUTPUT=$3
-
-echo 'zcat '$INPUT'| ./foliage sfilter' '--pt' $PATTERN '| gzip -c >' $OUTPUT |bash
-
